60571d7391f55e12b986a46fb943df87d70ff26b
Shraddha Thyagarajan <shraddhathyagarajan@Shraddhas-MacBook-Pro.local>
2022-11-27 12:34:49 -0500

    Initialized Sites with data values

---
+++ src/components/Data.java
@@ -0,0 +1,28 @@
+package components;
+
+import java.util.ArrayList;
+
+public class Data {
+    String varName;
+    int value;
+
+
+    
+    public Data(String varName, int value) {
+        this.varName = varName;
+        this.value = value;
+    }
+
+    public String getVarName() {
+        return this.varName;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+
+
+   
+    
+}
